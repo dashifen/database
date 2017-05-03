cb34524cf00ec518a3a27774f5e02f5d43056841
David Dashifen Kees <dashifen@dashifen.com>
2017-05-03 15:12:52 -0400

    getMap should fetch[Group|Pairs]

--- src/AbstractDatabase.php
+++ src/AbstractDatabase.php
@@ -205,7 +205,29 @@ QUERY;
 	public function getMap(string $query, array $criteria = []): array {
 		try {
 			$results = $this->dbConn->fetchGroup($query, $criteria);
-			return is_array($results) ? $results : [];
+			
+			// here's where we differ a bit from the underlying ExtendedPdo
+			// object.  my version of getting a map should return key/value
+			// pairs when there's only one value, and key/[value] pairs when
+			// there's more than one.  first, we'll bail out if we didn't get
+			// anything.
+			
+			if (!is_array($results)) {
+				return [];
+			}
+			
+			// now we know we got at least one thing from the database.  we'll
+			// look at it and see if it's alone or if it has friends.  if it's
+			// alone, we'll remove the inner arrays to produce simple x/y pairs
+			// suitable for hash table style lookup.
+			
+			foreach ($results as &$result) {
+				if (sizeof($results) === 1) {
+					$result = $result[0];
+				}
+			}
+			
+			return $results;
 		} catch (\PDOException $pdoException) {
 			throw $this->prepareDatabaseException($pdoException, $query, $criteria);
 		}
